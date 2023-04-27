88e6f599adf238b3078e46c2f5c2e620d3dea6de
YunBeomOh <dbsqja353@naver.com>
2023-04-27 17:16:14 +0900

    23_04_27 Day63 MovieFinder_Complete

---
+++ Day07/Day07WPFApp/wpf12_finedustCheck/Models/DustSensor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf12_finedustCheck.Models
+{
+    public class DustSensor
+    {
+        public int Id { get; set; }
+        public string Dev_id { get; set; }
+        public string Name { get; set; }
+        public string Loc { get; set; }
+        public double Coordx { get; set; }
+        public double Coordy { get; set; }
+        public bool Ison { get; set; }
+        public int Pm10_after { get; set; }
+        public int Pm25_after { get; set; }
+        public int State { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Company_id { get; set; }
+        public string Company_name { get; set; }
+    }
+}
